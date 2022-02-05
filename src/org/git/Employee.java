000855d5dc002bbcc329f3d9177264c813dee5cd
Jancygit19 <jancydev1987@gmail.com>
2022-02-05 21:31:50 +0530

    This is my folder

---
+++ src/org/git/Employee.java
@@ -0,0 +1,14 @@
+package org.git;
+
+public class Employee {
+	public void add() {
+		System.out.println("add");
+	}
+	public static void main(String[] args) {
+		Employee emp = new Employee();
+		emp.add();
+	}
+
+
+	
+}
